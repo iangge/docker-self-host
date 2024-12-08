cdc17b3f63df4e3de579d614cd815c179ef41093
iangge <87641560+iangge@users.noreply.github.com>
2024-12-08 15:57:07 -0800

    Update portforward-wsl2-to-lan.ps1
    Added  comments explaining Powershell commands and a command to view all forwarded ports.

--- portforward-wsl2-to-lan.ps1
+++ portforward-wsl2-to-lan.ps1
@@ -1,5 +1,13 @@
 # Source: https://learn.microsoft.com/en-us/windows/wsl/networking#accessing-a-wsl-2-distribution-from-your-local-area-network-lan
 
 netsh interface portproxy add v4tov4 listenport=<yourPortToForward> listenaddress=0.0.0.0 connectport=<yourPortToConnectToInWSL> connectaddress=(wsl hostname -I)
-
+# Enables port forwarding from Windows host machine port to WSL2 environment port
 # Update yourPortToForward. Ex: listenport=3000
+# Update yourPortToConnectToInWSL Ex: 192.168.x.x 
+
+##########
+
+# Source: https://jwstanly.com/blog/article/Port+Forwarding+WSL+2+to+Your+LAN/
+
+netsh interface portproxy show v4tov4
+# View forwarded ports in Powershell with the above command
